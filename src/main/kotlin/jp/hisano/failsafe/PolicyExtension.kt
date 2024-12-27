59d1b32f072a9b9586ebf07bc99b430593e741ad
Koji Hisano <hisano@gmail.com>
2024-12-27 11:04:03 +0900

    Add extensions to Policy class

---
+++ src/main/kotlin/jp/hisano/failsafe/PolicyExtension.kt
@@ -0,0 +1,10 @@
+package jp.hisano.jp.hisano.failsafe
+
+import dev.failsafe.ExecutionContext
+import dev.failsafe.Failsafe.with
+import dev.failsafe.FailsafeExecutor
+import dev.failsafe.Policy
+
+operator fun <R> Policy<R>.invoke(block: ExecutionContext<R>.() -> R): R = with(this).invoke(block)
+
+operator fun <R> Policy<R>.plus(block: Policy<R>): FailsafeExecutor<R> = with(this).compose(block)
---
+++ src/test/kotlin/jp/hisano/failsafe/PolicyExtensionTest.kt
@@ -0,0 +1,39 @@
+package jp.hisano.failsafe
+
+import dev.failsafe.RetryPolicy
+import jp.hisano.jp.hisano.failsafe.invoke
+import jp.hisano.jp.hisano.failsafe.plus
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class PolicyExtensionTest {
+    @Test
+    fun testInvoke() {
+        val retry = RetryPolicy.builder<Boolean>().withMaxRetries(5).build()
+
+        val result = retry {
+            if (5 <= attemptCount) {
+                true
+            } else {
+                throw IllegalStateException()
+            }
+        }
+
+        assertTrue(result)
+    }
+
+    @Test
+    fun testPlus() {
+        val retry = RetryPolicy.builder<Boolean>().withMaxRetries(5).build()
+
+        val result = (RetryPolicy.ofDefaults<Boolean>() + retry) {
+            if (5 <= attemptCount) {
+                true
+            } else {
+                throw IllegalStateException()
+            }
+        }
+
+        assertTrue(result)
+    }
+}
